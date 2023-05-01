383d5a71f0e871142b2320f0a43efaed6dd51fbe
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-05-01 16:31:43 -0300

    update user controller

---
+++ lib/app/modules/user/update-user/presenter/controller/update_user_controller.dart
@@ -0,0 +1,83 @@
+import 'package:mobx/mobx.dart';
+
+import '../../../../../../generated/l10n.dart';
+import '../../../../../../shared/domain/usecases/update_user_usecase.dart';
+import '../../states/update_user_state.dart';
+
+part 'update_user_controller.g.dart';
+
+class UpdateUserController = UpdateUserControllerBase
+    with _$UpdateUserController;
+
+abstract class UpdateUserControllerBase with Store {
+  final IUpdateUserUsecase _updateUserUsecase;
+
+  UpdateUserControllerBase(this._updateUserUsecase);
+
+  @observable
+  String userName = '';
+
+  @observable
+  String userEmail = '';
+
+  @observable
+  String userPassword = '';
+
+  @observable
+  String userId = '';
+
+  @observable
+  UpdateUserState state = const StartUpdateState();
+
+  @action
+  setUserName(String name) => userName = name;
+
+  @action
+  setUserEmail(String email) => userEmail = email;
+
+  @action
+  setUserPassword(String password) => userPassword = password;
+
+  @action
+  setUserId(String id) => userId = id;
+
+  @action
+  setPageState(UpdateUserState value) => state = value;
+
+  @action
+  Future<void> updateUser() async {
+    setPageState(const LoadingUpdateState());
+    var result =
+        await _updateUserUsecase(userName, userEmail, userPassword, userId);
+    setPageState(
+        result.fold((l) => ErrorUpdateState(l), (r) => SuccessUpdateState(r)));
+  }
+
+  @action
+  String? validateUserName(String? value) {
+    if (value!.isEmpty) {
+      return S.current.fieldRequired;
+    }
+    return null;
+  }
+
+  @action
+  String? validateUserEmail(String? value) {
+    if (value!.isEmpty) {
+      return S.current.fieldRequired;
+    } else if (!value.contains('@')) {
+      return S.current.fieldInvalidEmail;
+    }
+    return null;
+  }
+
+  @action
+  String? validateUserPassword(String? value) {
+    if (value!.isEmpty) {
+      return S.current.fieldRequired;
+    } else if (value.length < 6) {
+      return S.current.fieldMinLength;
+    }
+    return null;
+  }
+}
--- lib/shared/domain/usecases/update_user_usecase.dart
+++ lib/shared/domain/usecases/update_user_usecase.dart
@@ -3,10 +3,12 @@ import 'package:dartz/dartz.dart';
 import '../../helpers/errors/errors.dart';
 import '../../infra/models/user_model.dart';
 import '../entities/user.dart';
+import '../enums/state_enum.dart';
 import '../repositories/user_repository_interface.dart';
 
 abstract class IUpdateUserUsecase {
-  Future<Either<Failure, User>> call(UserModel user);
+  Future<Either<Failure, User>> call(
+      String name, String email, String password, String id);
 }
 
 class UpdateUserUsecase implements IUpdateUserUsecase {
@@ -15,7 +17,15 @@ class UpdateUserUsecase implements IUpdateUserUsecase {
   UpdateUserUsecase(this.repository);
 
   @override
-  Future<Either<Failure, User>> call(UserModel user) async {
-    return await repository.updateUser(user);
+  Future<Either<Failure, User>> call(
+      String name, String email, String password, String id) async {
+    UserModel userToUpdate = UserModel(
+      email: email,
+      name: name,
+      password: password,
+      state: StateEnum.REJECTED,
+      id: id,
+    );
+    return await repository.updateUser(userToUpdate);
   }
 }

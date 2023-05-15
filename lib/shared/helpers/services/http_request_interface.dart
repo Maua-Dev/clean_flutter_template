4fa0f8b4d190d1d49ba8660dbe36e4603114f42a
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-05-15 16:28:19 -0300

    services TU

--- lib/shared/helpers/services/http_request_interface.dart
+++ lib/shared/helpers/services/http_request_interface.dart
@@ -4,4 +4,5 @@ abstract class IHttpRequest {
   Future<Response> get(String url);
   Future<Response> post(String url, dynamic data);
   Future<Response> put(String url, dynamic data);
+  void setAuthorizationToken(String authorizationToken);
 }
